ce71b0a2560707fc85586e87ce1a58718a1bcdaf
mattstruble <mattstruble@outlook.com>
2013-05-05 19:42:57 -0700

    Initial attempt at evolutionary computation.

---
+++ Evolution/Evolution/Evolution/Files/Block.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+
+namespace Evolution.Files
+{
+	class Block
+	{
+		private Color _color;
+
+		private Random _rand;
+
+		private Vector2 _location;
+		private Vector2 _size;
+
+		public Block()
+		{
+			_rand = new Random();
+
+			_color = new Color(_rand.Next(0, 255), _rand.Next(0, 255), _rand.Next(0, 255));
+
+			_location = new Vector2(0);
+			_size = new Vector2(0);
+		}
+
+		public Block(Vector2 loc, Vector2 s)
+		{
+			_rand = new Random();
+
+			_color = new Color(_rand.Next(0, 255), _rand.Next(0, 255), _rand.Next(0, 255));
+
+			_location = loc;
+			_size = s;
+		}
+
+		public Block(Color c)
+		{
+			_color = c;
+
+			_location = new Vector2(0);
+			_size = new Vector2(0);
+		}
+
+		public Block(Vector3 c)
+		{
+			_color = new Color(c);
+
+			_location = new Vector2(0);
+			_size = new Vector2(0);
+		}
+
+		public Block(float r, float g, float b)
+		{
+			_color = new Color(r, g, b);
+		}
+
+		public Block(Color c, Vector2 loc, Vector2 s)
+		{
+			_color = c;
+			_location = loc;
+			_size = s;
+		}
+
+		public Block(Vector3 cV, Vector2 loc, Vector2 s)
+		{
+			_color = new Color(cV);
+			_location = loc;
+			_size = s;
+		}
+
+		public Block(float r, float g, float b, Vector2 loc, Vector2 s)
+		{
+			_color = new Color(r, g, b);
+			_location = loc;
+			_size = s;
+		}
+
+		public void setColor(float r, float g, float b)
+		{
+			_color = new Color(r, g, b);
+		}
+
+		public void setColor(Color c)
+		{
+			_color = c;
+		}
+
+		public void setColor(Vector3 c)
+		{
+			_color = new Color(c);
+		}
+
+		public void setLoc(Vector2 l)
+		{
+			_location = l;
+		}
+
+		public void setSize(Vector2 s)
+		{
+			_size = s;
+		}
+
+		public Color getColor()
+		{
+			return _color;
+		}
+
+		public Vector2 getLoc()
+		{
+			return _location;
+		}
+
+		public Vector2 getSize()
+		{
+			return _size;
+		}
+
+		public Vector3 getColorV()
+		{
+			return _color.ToVector3();
+		}
+
+		public float[] getGene()
+		{
+			Vector3 tmpV3 = _color.ToVector3();
+
+			float[] gene = new float[] { tmpV3.X, tmpV3.Y, tmpV3.Z };
+
+			return gene;
+		}
+
+		public void draw(SpriteBatch pBatch, Texture2D assetSheet)
+		{
+			pBatch.Draw(assetSheet,
+				_location,
+				new Rectangle(0, 0, 1, 1),
+				_color, 
+				0f, 
+				new Vector2(0),
+				_size,
+				SpriteEffects.None,
+				0f);
+		}
+
+	}
+}
---
+++ Evolution/Evolution/Evolution/Files/BlockManager.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Evolution.Files
+{
+	class BlockManager
+	{
+		private List<Block> _blocks;
+		private List<Block> _matingPool;
+
+		private Color _targetColor;
+		private Block _targetBlock;
+
+		private Vector2 _blockSize;
+		private Vector3 _tColorVector;
+
+		private Random _rand;
+
+		private Texture2D _assetSheet;
+
+		private int _timeUntilUpdate;
+		private int _updateTime;
+
+		public BlockManager()
+		{
+			_blocks = new List<Block>();
+			_matingPool = new List<Block>();
+			_rand = new Random();
+
+			_blockSize = new Vector2(50);
+
+			_updateTime = _timeUntilUpdate = 5000;
+		}
+
+		public void loadContent(ContentManager pContent)
+		{
+			_assetSheet = pContent.Load<Texture2D>("blank");
+		}
+
+		public void generate(int num)
+		{
+			Color tmpC;
+
+			for (int i = 0; i < num; i++)
+			{
+				tmpC = new Color(_rand.Next(0, 255), _rand.Next(0, 255), _rand.Next(0, 255));
+
+				_blocks.Add(new Block(tmpC, new Vector2(_blockSize.X * i, 100), _blockSize));
+			}
+
+			_targetColor = new Color(_rand.Next(0, 255), _rand.Next(0, 255), _rand.Next(0, 255));
+			_tColorVector = _targetColor.ToVector3();
+
+			_targetBlock = new Block(_targetColor, new Vector2(200, 200), _blockSize);
+		}
+
+		public void update(GameTime gameTime)
+		{
+			if (_timeUntilUpdate <= 0 && _blocks.Count > 0)
+			{
+				generateMatingPool();
+				_timeUntilUpdate = _updateTime;
+			}
+			else if (_timeUntilUpdate <= 0 && _matingPool.Count > 0)
+			{
+				crossOver();
+				_timeUntilUpdate = _updateTime;
+			}
+
+			_timeUntilUpdate -= gameTime.ElapsedGameTime.Milliseconds;
+		}
+
+		public void draw(SpriteBatch pBatch)
+		{
+			if (_blocks.Count > 0)
+			{
+				for (int i = 0; i < _blocks.Count; i++)
+				{
+					_blocks[i].draw(pBatch, _assetSheet);
+				}
+			}
+			else if (_matingPool.Count > 0)
+			{
+				for (int i = 0; i < _matingPool.Count; i++)
+				{
+					_matingPool[i].draw(pBatch, _assetSheet);
+				}
+			}
+
+			_targetBlock.draw(pBatch, _assetSheet);
+		}
+
+		//EVOLUTIONARY FUNCTIONS BELOW
+
+		private void generateMatingPool()
+		{
+			Block b1, b2;
+
+			while (_blocks.Count > 0)
+			{
+				b1 = _blocks[_rand.Next(_blocks.Count)];
+
+				do
+				{
+					b2 = _blocks[_rand.Next(_blocks.Count)];
+				} while (b2 == b1);
+
+				if (fitness(b1) < fitness(b2))
+					_matingPool.Add(b1);
+				else
+					_matingPool.Add(b2);
+
+				_blocks.Remove(b1);
+				_blocks.Remove(b2);
+			}
+		}
+
+		private float fitness(Block b)
+		{
+			float fit = 0;
+			Vector3 color = b.getColorV();
+
+			fit = Math.Abs(color.X - _tColorVector.X) + Math.Abs(color.Y - _tColorVector.Y) + Math.Abs(color.Z - _tColorVector.Z);
+
+			return fit;
+		}
+
+		private void crossOver()
+		{
+			Block b1, b2;
+			float[] g1; float[] g2;
+			float[] tg1; float[] tg2;
+			int swapPoint;
+
+			while (_matingPool.Count > 0)
+			{
+				b1 = _matingPool[0];
+				_matingPool.Remove(b1);
+
+				b2 = _matingPool[0];
+				_matingPool.Remove(b2);
+
+				g1 = tg1 = b1.getGene();
+				g2 = tg2 = b2.getGene();
+
+				swapPoint = tg1.Length;
+
+				for(int i = 0; i < tg1.Length; i ++)
+				{
+					if(i == swapPoint)
+					{
+						g1[i] = tg2[i];
+						g2[i] = tg1[i];
+					}
+
+					if (_rand.Next(0, 100) < 20)
+						if (_rand.Next(0, 100) < 50)
+							g1[i] += .1f;
+						else
+							g1[i] -= .1f;
+
+					if (_rand.Next(0, 100) < 20)
+						if (_rand.Next(0, 100) < 50)
+							g2[i] += .1f;
+						else
+							g2[i] -= .1f;
+
+				}
+
+				_blocks.Add(new Block(convertToV3(g1)));
+				_blocks.Add(new Block(convertToV3(g2)));
+				_blocks.Add(b1);
+				_blocks.Add(b2);
+				
+			}
+
+			fixBlocks();
+		}
+
+		private void fixBlocks()
+		{
+			for (int i = 0; i < _blocks.Count; i++)
+			{
+				_blocks[i].setLoc(new Vector2(i * _blockSize.X, 100));
+				_blocks[i].setSize(_blockSize);
+			}
+		}
+
+		private Vector3 convertToV3(float[] arr)
+		{
+			return new Vector3(arr[0], arr[1], arr[2]);
+		}
+	}
+}
